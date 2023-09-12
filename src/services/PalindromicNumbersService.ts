930e8c21dcade7a1f013bb8e5aa50a218868321e
Sallys Carlos <sallys.carlos93@gmail.com>
2023-09-12 17:48:18 -0300

    feat: created palindromicNumbersService

---
+++ src/services/PalindromicNumbersService.ts
@@ -0,0 +1,26 @@
+interface IPalindromicNumbersService {
+  start: number;
+  end: number;
+}
+
+class PalindromicNumbersService {
+  execute({ start, end }: IPalindromicNumbersService) {
+    const numbers = new Array<Number>;
+
+    for (let index = start; index <= end; index++) {
+      let number = index;
+      let palindromic = '';
+
+      while (number != 0) {
+        palindromic = palindromic.concat((number % 10).toString());
+        number = Math.trunc(number / 10);
+      }
+
+      if (parseInt(palindromic) === index) {
+        numbers.push(index);
+      }
+    }
+
+    return numbers;
+  }
+}
