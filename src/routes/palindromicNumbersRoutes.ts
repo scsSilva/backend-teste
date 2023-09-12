893b82bba79a5d3ff30c48c3c5a4ca06132c7b25
Sallys Carlos <sallys.carlos93@gmail.com>
2023-09-12 18:17:47 -0300

    feat: added palindromicNumbersRoutes

---
+++ src/routes/palindromicNumbersRoutes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import { PalindromicNumbersController } from '../controllers/PalindromicNumbersController';
+
+export const palindromicNumbersRoutes = Router();
+
+const palindromicNumbersController = new PalindromicNumbersController();
+
+palindromicNumbersRoutes.get('/palindromicNumbers/list', palindromicNumbersController.list);
