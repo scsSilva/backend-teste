0096b9d1c936bc9f9446c330c57076ff71082f12
Sallys Carlos <sallys.carlos93@gmail.com>
2023-09-12 17:40:06 -0300

    feat: created cashFlowService

---
+++ src/services/CashFlowService.ts
@@ -0,0 +1,21 @@
+interface ICashFlowService {
+  price: number;
+  amountReceived: number;
+}
+
+class CashFlowService {
+  execute({ price, amountReceived }: ICashFlowService) {
+    let change = amountReceived - price;
+
+    const hundredNotes = Math.trunc(change / 100);
+    change %= 100;
+
+    const notesOutOfTen = Math.trunc(change / 10);
+    change %= 10;
+
+    const notesOutOfOne = change;
+    change += (notesOutOfTen * 10) + (hundredNotes * 100);
+
+    return { price, amountReceived, change, hundredNotes, notesOutOfTen, notesOutOfOne };
+  }
+}
