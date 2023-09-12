293e262da862cf6eadc8c316dc40e5de857f013f
Sallys Carlos <sallys.carlos93@gmail.com>
2023-09-12 18:20:12 -0300

    feat: added app

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { json } from 'express';
+import cors from 'cors';
+import { routes } from './routes';
+
+const app = express();
+
+app.use(json());
+app.use(cors());
+app.use(routes);
+
+app.listen(3000);
