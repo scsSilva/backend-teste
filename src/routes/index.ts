e6392da228755694df881f006b54f91c9cf49d57
Sallys Carlos <sallys.carlos93@gmail.com>
2023-09-13 13:30:09 -0300

    feat: added garageRoutes in routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
-import { Router } from 'express';
-import { cashFlowRoutes } from './cashFlowRoutes';
-import { palindromicNumbersRoutes } from './palindromicNumbersRoutes';
+import { Router } from "express";
+import { cashFlowRoutes } from "./cashFlowRoutes";
+import { palindromicNumbersRoutes } from "./palindromicNumbersRoutes";
+import { garageRoutes } from "./garageRoutes";
 
 export const routes = Router();
 
 routes.use(cashFlowRoutes);
 routes.use(palindromicNumbersRoutes);
+routes.use(garageRoutes);
